0f6be6d195e3fee93b219666d9fc445266eb9354
Fyne Tonye Angala <angalafynetonye@gmail.com>
2025-06-10 00:03:38 +0100

    added a project

--- constants/projects.js
+++ constants/projects.js
@@ -42,3 +42,61 @@ export const featuredProjects = [
   },
 ];
 
+export const projects = [
+  {
+    name: "Movie App",
+    detail:
+      "Movie App is a web App that showing trending and high ranked movies, consuming externa api and displaying for users.",
+    tech: ["Next.js", "Tailwind CSS"],
+    webUrl: "https://viv-react-movie-app.netlify.app/",
+    githubUrl: "",
+  },
+  {
+    name: "Grocery Bud",
+    detail:
+      "Grocery Bud is a mini web app that a user can input item they bought and can edit later or delete. It practically demostrate CRUD operation on the frontend.",
+    tech: ["React.js", "Tailwind CSS", "Redux"],
+    webUrl: "https://viv-react-grocery-bud.netlify.app/",
+    githubUrl: "",
+  },
+  {
+    name: "Cocktail DB",
+    detail:
+      "Cocktai DB is a mini web app that consumes a cocktail external api, get a list of cocktail and displays them, A user can access each cocktail to know more information about and how to prepare it. ",
+    tech: ["React.js", "Vanilla CSS ", "Context api"],
+    webUrl: "https://viv-cocktail.netlify.app/",
+    githubUrl: "",
+  },
+  {
+    name: "Clone Stripe Homepage",
+    detail:
+      "A clone stripe page displaying the iteracativity and a unique animated navbar and a nice mobile responsiveness",
+    tech: ["React.js", "SASS", ],
+    webUrl: "https://viv-react-stripe-menu.netlify.app/",
+    githubUrl: "",
+  },
+  {
+    name: "Modern UI design",
+    detail:
+      "A modern UI design with reactjs. ",
+    tech: ["Reactjs",  "Tailwind CSS"],
+    webUrl: "https://viv-modern-ui.netlify.app/",
+    githubUrl: "",
+  },
+  // {
+  //   name: "Rotobot AI",
+  //   detail:
+  //     "Rotobot AI serves as a platform to inform users about JosReady, potentially a service or product, and its kick-off event. This information could include details about the event, its purpose, and how users can participate",
+  //   tech: ["Next.js", "Tailwind CSS"],
+  //   webUrl: "https://rotobot.ai/",
+  //   githubUrl: "",
+  // },
+  // {
+  //   name: "NterNow",
+  //   detail:
+  //     "NterNow serves as a platform for showcasing various properties for sale in different locations. Users can explore a range of properties with different features and sizes, catering to diverse preferences and budgets.",
+  //   tech: ["React.js", "Bootstrap", "Google Maps"],
+  //   webUrl: "https://nternow-tournow-testpage.vercel.app/cbh%20homes",
+  //   githubUrl: "",
+  // },
+];
